78f502c136acb63ef22fe04f301029d3e2643225
mukhammadrizo2009 <muhammadrizomirzaev671@gmail.com>
2026-03-31 19:19:35 +0500

    fix(static): use resilient CompressedStaticFilesStorage to avoid manifest errors

--- core/settings.py
+++ core/settings.py
@@ -149,7 +149,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
